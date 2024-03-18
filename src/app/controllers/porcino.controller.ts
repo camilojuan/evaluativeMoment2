c64b6a4741c7ab4f74da8e702b146001a1beab0b
Camilo <camilongo_96@hotmail.com>
2024-03-18 14:09:36 -0500

    se comunico el controllador de porcinos con el modelo y el dto

--- src/app/controllers/porcino.controller.ts
+++ src/app/controllers/porcino.controller.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
-
+import { PorcinoModel } from "../model/porcino";
+import { PorcinoDTO } from "../helpers/DTO/porcinoDTO";
 @Component({
     selector: 'app-porcino',
     templateUrl:'../view/porcino.html'
